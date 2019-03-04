1cdbe27ae4bd33f433f8cd547cd9345f822ce4aa
Girish Patel <patelg123@gmail.com>
2019-03-04 15:48:58 +0000

    changed newsitemdetails component back to class

--- client/src/components/presentation/NewsItemDetail.js
+++ client/src/components/presentation/NewsItemDetail.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 //import PropTypes from 'prop-types';
 
-const NewsItemDetail = ()=>(
+class NewsItemDetail extends Component {
+    render() {
+        return (
             <div>
                 <h2>News story title</h2>
                 <p>Body: test Mad Owl story text {this.props.id}</p>
             </div>
-);
+        );
+    }
+}
 
 export default NewsItemDetail;
